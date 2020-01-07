25313901614d81f4c975ec1e16b3b14f58d84bd0
mac <mac@dhcp-10-105-20-203.wireless.northwestern.private>
2020-01-06 23:21:35 -0600

    Filtering

--- src/App.js
+++ src/App.js
@@ -2,31 +2,7 @@ import React, { useState, useEffect } from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": [
-    {
-      "id": "F101",
-      "title": "Computer Science: Concepts, Philosophy, and Connections",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F110",
-      "title": "Intro Programming for non-majors",
-      "meets": "MWF 10:00-10:50"
-    },
-    {
-      "id": "F111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 13:00-13:50"
-    },
-    {
-      "id": "F211",
-      "title": "Fundamentals of Computer Programming II",
-      "meets": "TuTh 12:30-13:50"
-    }
-  ]
-};
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
 const Banner = ({ title }) => (
     <Title>{ title }</Title>
 );
@@ -72,6 +48,7 @@ const getCourseTerm = course => (
 const getCourseNumber = course => (
     course.id.slice(1, 4)
 )
+
 const daysOverlap = (days1, days2) => (
     days.some(day => days1.includes(day) && days2.includes(day))
 );
