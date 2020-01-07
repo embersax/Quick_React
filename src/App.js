fc5905a43e682c90f5de6df7d1586a503d9aedf7
mac <mac@dhcp-10-105-20-203.wireless.northwestern.private>
2020-01-06 23:13:50 -0600

    Filtering

--- src/App.js
+++ src/App.js
@@ -49,12 +49,16 @@ const TermSelector = ({ state }) => (
 );
 const CourseList = ({ courses }) => {
   const [term, setTerm] = useState('Fall');
+  const [selected, toggle] = useSelection();
   const termCourses = courses.filter(course => term === getCourseTerm(course));
+
   return (
       <React.Fragment>
         <TermSelector state={ { term, setTerm } } />
         <Button.Group>
-          { termCourses.map(course => <Course key={ course.id } course={ course } state={ { term, setTerm } } />) }
+          { termCourses.map(course =>
+              <Course key={ course.id } course={ course }
+                      state={ { selected, toggle } } />) }
         </Button.Group>
       </React.Fragment>
   );
@@ -68,13 +72,61 @@ const getCourseTerm = course => (
 const getCourseNumber = course => (
     course.id.slice(1, 4)
 )
+const daysOverlap = (days1, days2) => (
+    days.some(day => days1.includes(day) && days2.includes(day))
+);
 
-const Course = ({ course }) => (
-    <Button>
+const hoursOverlap = (hours1, hours2) => (
+    Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
+
+const timeConflict = (course1, course2) => (
+    daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+
+const courseConflict = (course1, course2) => (
+    course1 !== course2
+    && getCourseTerm(course1) === getCourseTerm(course2)
+    && timeConflict(course1, course2)
+);
+const hasConflict = (course, selected) => (
+    selected.some(selection => courseConflict(course, selection))
+);
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: schedule.courses.map(addCourseTimes)
+});
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+const Course = ({ course, state }) => (
+    <Button color={ buttonColor(state.selected.includes(course)) }
+            onClick={ () => state.toggle(course) }
+            disabled={ hasConflict(course, state.selected) }
+    >
       { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
     </Button>
 );
-
+const useSelection = () => {
+  const [selected, setSelected] = useState([]);
+  const toggle = (x) => {
+    setSelected(selected.includes(x) ? selected.filter(y => y !== x) : [x].concat(selected))
+  };
+  return [ selected, toggle ];
+};
 const App = () => {
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
   const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
@@ -84,7 +136,7 @@ const App = () => {
       const response = await fetch(url);
       if (!response.ok) throw response;
       const json = await response.json();
-      setSchedule(json);
+      setSchedule(addScheduleTimes(json));
     }
     fetchSchedule();
   }, [])
