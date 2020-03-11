1b4c2439e8f76870f6985b1c28d0846d4d9ccfea
embersax <ruilinzhou2022@u.northwestern.edu>
2020-03-10 21:40:54 -0500

    test launching

--- src/component/Course/Course.js
+++ src/component/Course/Course.js
@@ -1,17 +1,17 @@
-import {Button} from "rbx";
+import { Button } from "rbx";
 import React from "react";
-import {getCourseTerm,hasConflict,getCourseNumber,timeParts} from "./times";
-import {db} from "./index";
+import { getCourseTerm, hasConflict, getCourseNumber, timeParts } from "./times";
+import { db } from "./index";
 
 const saveCourse = (course, meets) => {
-    db.child('courses').child(course.id).update({meets})
+    db.child('courses').child(course.id).update({ meets })
         .catch(error => alert(error));
 };
 
 const moveCourse = course => {
     const meets = prompt('Enter new meeting data, in this format:', course.meets);
     if (!meets) return;
-    const {days} = timeParts(meets);
+    const { days } = timeParts(meets);
     if (days) saveCourse(course, meets);
     else moveCourse(course);
 };
@@ -21,12 +21,15 @@ export const buttonColor = selected => (
 )
 
 const Course = ({ course, state, user }) => (
-    <Button color={ buttonColor(state.selected.includes(course)) }
-            onClick={ () => state.toggle(course) }
-            onDoubleClick={ user ? () => moveCourse(course) : null }
-            disabled={ hasConflict(course, state.selected) }
+    <Button data-cy="course"
+
+        color={buttonColor(state.selected.includes(course))}
+        onClick={() => state.toggle(course)}
+        onDoubleClick={user ? () => moveCourse(course) : null}
+        disabled={hasConflict(course, state.selected)}
+
     >
-        { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+        {getCourseTerm(course)} CS {getCourseNumber(course)}: {course.title}
     </Button>
 );
 
